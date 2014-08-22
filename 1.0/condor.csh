de6bf65bd4aff5714a9c4c85efae8605ec37b000
Anton Yu <antonyu@midway-login1.rcc.local>
2014-08-22 10:23:17 -0500

    removed local.bosco

--- 1.0/condor.csh
+++
@@ -1,3 +0,0 @@
-# The script should be sourced by /bin/csh or similar
-setenv CONDOR_CONFIG "$HOME/bosco/etc/condor_config"
-setenv PATH "$HOME/bosco/bin:$HOME/bosco/sbin:$PATH"
--- 1.0/condor.sh
+++
@@ -1,5 +0,0 @@
-# The script should be sourced by /bin/sh or similar
-CONDOR_CONFIG="$HOME/bosco/etc/condor_config"
-export CONDOR_CONFIG
-PATH="$HOME/bosco/bin:$HOME/bosco/sbin:$PATH"
-export PATH
