39756c8298a7b309ad8fc8211f4b87c2b8c55e1a
David Champion <dgc@uchicago.edu>
2014-10-21 11:26:48 -0500

    clean up order and comments

--- install.sh
+++ install.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
-# Need to adopt Suchandra's idea for installing - perhaps this variation:
-# - find . -name install.sub -print
-# - source each of these in turn in a subshell (to inherit install.sh's
-#   context but insulate the install.sub from one another)
-
-from=$(dirname "$0")
-base="$1"
-modlib="$2"
-
 if [ -z "$base" -o -z "$modlib" -o "$base" = "-h" -o "$base" = "--help" ]; then
 	echo >&2 "usage: $0 software-install-directory modulefiles-directory"
 	exit 2
 fi
 
+## Setup stuff.
+## Actual installation steps are below.
+## All these variables and functions are exposed to install.sub files.
+
+from=$(dirname "$0")
+base="$1"
+modlib="$2"
+
 copyfiles () {
 	rsync -a --exclude install.sub "$@"
 }
-
-cd $(dirname "$0")
-mkdir -p "$base" 2>/dev/null
-mkdir -p "$base/bin" 2>/dev/null
-mkdir -p "$modlib/connect" 2>/dev/null
 
 status () {
 	echo "[install]" "$@"
@@ -34,17 +28,30 @@ subinstall () {
 	)
 }
 
+
+## Installation procedure:
+
+cd $(dirname "$0")
+mkdir -p "$base" 2>/dev/null
+mkdir -p "$base/bin" 2>/dev/null
+mkdir -p "$modlib/connect" 2>/dev/null
+
 if [ ! -d "$base" ]; then
 	echo >&2 "Cannot create $base - cannot install."
 	exit 10
 fi
 
+# Install mofulefiles
 subinstall modules
+
+# Install bosco (connect client)
 subinstall bosco
 
+# Install connect scripts
 status Installing Connect user commands
 subinstall connect
+
 # tutorial has no install.sub because it's a subrepo
 status ... tutorial command
-cp -p scripts/tutorial/tutorial "$base/bin/"
+copyfiles scripts/tutorial/tutorial "$base/bin/"
 
