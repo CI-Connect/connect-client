95b3657fe7bd46c633e911eed6f4cf822bb2c37d
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-23 16:37:50 -0500

    Initial switch command commit

---
+++ switch-module/switch_modules.sh
@@ -0,0 +1,28 @@
+switch_modules () {
+
+  shell=`basename \`ps -p $$ -ocomm=\``
+  case $1 in 
+  oasis) 
+    . /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/$shell
+    ;;
+  local)
+     . /srv/adm/modules/init/$shell
+    ;;
+  esac
+}
+
+unload_modules () {
+  next_token=0
+  for i in `module list 2>&1`;
+  do
+    if [[ "$next_token" == "1" ]];
+    then
+      module unload $i
+      next_token=0
+    fi
+    if grep -q ')' <<< $i;
+    then
+      next_token=1
+    fi
+done
+}
