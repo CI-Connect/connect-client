e29c0b9f9ef750e6836fe09e4ea33c4f53df38e0
David Champion <dgc@uchicago.edu>
2015-07-06 12:38:41 -0500

    add release script

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+usage () {
+	echo >&2 "usage: $0 version"
+}
+
+if [ "$1" = "" ]; then
+	echo >&2 "no version given!"
+	usage
+	exit 2
+fi
+
+case "$1" in
+	v*)	:
+		;;
+	*)	:
+		echo >&2 "'$1' not a version string (must begin with 'v')"
+		usage
+		exit 2
+		;;
+esac
+
+rel="$1"
+
+dir=$(dirname "$0")
+cd "$dir"
+echo "$rel" >.version
+git ci -a "tag release $rel" .version
+git tag -a "release tag $rel" $rel
