2fbd42646f23493cc11529a3b26b09944bfd4740
Suchandra Thapa <sthapa@ci.uchicago.edu>
2018-03-27 15:42:15 -0500

    Update setup file

--- standalone-client/setup.py
+++ standalone-client/setup.py
@@ -4,11 +4,11 @@
 # Available under Apache 2.0 License
 from distutils.core import setup
 
-setup(name='connect_lib',
+setup(name='connect_client',
       version='1.0',
       description='Scripts for connect client',
       url='https://github.com/CI-Connect/connect-client.git',
-      packages=['connect-client'],
+      packages=['connect_lib'],
       scripts=['connect'],
       license='Apache 2.0')
 
